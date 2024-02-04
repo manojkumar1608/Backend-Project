19c7f2d56e17bad0d9adbb351f207bbb641743f6
manoj <pitlamanoj1@gmail.com>
2024-02-05 00:48:26 +0530

    adding cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
+
 
 dotenv.config({
 path:'./env'
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+    console.log(`*server is running at : ${process.env.PORT}`)
+})
+    app.on("error",(error)=>{
+        console.log("404 not Found",error)
+        throw error
+
+    })
+
+})
+.catch((error)=>{
+    console.error(`MONGODB CONNECTION FAILED`,error)
+
+})
