9e53f1fcf025f8ac9420d2f748282d528e0a43c5
manoj <pitlamanoj1@gmail.com>
2024-02-07 20:24:02 +0530

    adding utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,30 @@
+class ApiError  extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        super(message)
+        this.message = message
+        this.data = null
+        this.errors = errors
+        this.statusCode = statusCode
+        this.success = false
+    
+
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+        
+
+    }
+        
+
+    
+
+}
